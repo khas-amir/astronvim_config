df5a30164cc83d6b61f326d997ced471c22ce420
Amirkhan <dlordproudd@gmail.com>
2024-01-01 18:29:30 +0300

    Init

---
+++ options.lua
@@ -0,0 +1,19 @@
+return function(local_vim)
+
+  if  vim.fn.has('wsl') == 1 then
+    local_vim.g.clipboard = {
+      name = 'WslClipboard',
+      copy = {
+        ['+'] = 'clip.exe',
+        ['*'] = 'clip.exe',
+      },
+      paste = {
+        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      },
+      cache_enabled = 0,
+    }
+  end
+
+  return local_vim
+end
