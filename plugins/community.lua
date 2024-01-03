050674e355ad4cd03d2d846e0ebf138e35b1bda3
Amirkhan <dlordproudd@gmail.com>
2024-01-03 15:53:17 +0300

    feat: Change colorscheme to catppuccin

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,4 +2,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.completion.codeium-vim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }
